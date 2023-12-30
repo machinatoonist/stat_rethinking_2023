0159e2e61a0ae98c31c4a0877ac33609a1228202
Matt Rosinski <mattrosinski+1@gmail.com>
2023-12-30 14:17:47 +1000

    Plotting posterior predictions for GDP from ruggedness and continent

--- clean/ch8_exercise.R
+++ clean/ch8_exercise.R
@@ -1,4 +1,8 @@
 library(rethinking)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
 
 data("rugged")
 d <- rugged
@@ -12,7 +16,7 @@ dd = d[complete.cases(d$rgdppc_2000), ]
 ??complete.case()
 dim(dd)
 
-# rescale variables
+# rescale variables ----
 # log GDP is divided by the average value so it is a proportion of the 
 # international average.  1 means average, 0.8 means 80% of the average and
 # 1.1 means 10% more than average
@@ -21,6 +25,7 @@ dd$log_gdp_std = dd$log_gdp / mean(dd$log_gdp)
 # Scaling so 0 represents zero ruggedness and 1 represents the most rugged
 dd$rugged_std = dd$rugged / max(dd$rugged)
 
+# Fit model ----
 m8.1 = quap(
     alist(
         log_gdp_std ~ dnorm( mu, sigma),
@@ -31,7 +36,7 @@ m8.1 = quap(
     ), data = dd
 )
 
-# Look at prior predictions
+# Look at prior predictions ----
 set.seed(7)
 prior = extract.prior(m8.1)
 
@@ -63,3 +68,83 @@ m8.1 = quap(
 
 # No overall association between terrain ruggedness and log GDP
 precis(m8.1)
+
+# Make variable to index Africa (1) or not (2) ----
+dd$cid = ifelse(dd$cont_africa == 1, 1, 2)
+
+# Fit model that indexes on continent ----
+m8.2 = quap(
+    alist(
+        log_gdp_std ~ dnorm(mu, sigma),
+        mu <- a[cid] + b * (rugged_std - 0.215),
+        a[cid] ~ dnorm(1, 0.1),
+        b ~ dnorm(0, 0.3),
+        sigma ~ dexp(1)
+    ), data = dd
+)
+
+compare(m8.1, m8.2)
+
+precis(m8.2, depth = 2)
+
+
+# Posterior contrast between two intercepts ----
+post <- extract.samples(m8.2)
+post %>% glimpse()
+
+diff_a1_a2 = post$a[, 1] - post$a[, 2]
+rethinking::PI(diff_a1_a2, prob = 0.9)
+
+# Plot posterior predictions (retrodictions) ----
+rugged.seq = seq(from = -0.1, to = 1.1, length.out = 30)
+
+# compute mu over samples, fixing cid = 2 and then cid = 1
+mu.NotAfrica = link(m8.2, data = data.frame(cid = 2, rugged_std = rugged.seq))
+
+mu.Africa = link(m8.2, data = data.frame(cid = 1, rugged_std = rugged.seq))
+
+# summarise to means and intervals
+(mu.NotAfrica_mu = apply(mu.NotAfrica, 2, mean))
+(mu.Africa_mu = apply(mu.Africa, 2, mean))
+
+(mu.NotAfrica_ci = apply(mu.NotAfrica, 2, PI, prob = 0.97))
+(mu.Africa_ci = apply(mu.Africa, 2, PI, prob = 0.97))
+
+cbind(
+    dd[dd$cid == 1, c("log_gdp_std", "rugged_std")],
+    
+)
+dd[dd$cid == 1, c("log_gdp_std", "rugged_std")]
+
+dd = 
+    dd %>% 
+    mutate(cont = ifelse(cid == 1, "Africa", "Not Africa"))
+
+fit_data = 
+    rbind(
+        data.frame(rugged_std = rugged.seq,
+                   mu = mu.Africa_mu, 
+                   ci_low = mu.Africa_ci[1,],
+                   ci_high = mu.Africa_ci[2,]) %>% 
+            mutate(cont = "Africa"),
+        data.frame(rugged_std = rugged.seq,
+                   mu = mu.NotAfrica_mu,
+                   ci_low = mu.NotAfrica_ci[1,],
+                   ci_high = mu.NotAfrica_ci[2,]) %>% 
+            mutate(cont = "Not Africa")
+        )
+
+
+fit_data %>% 
+    ggplot(aes(x = rugged_std, y = mu, color = cont)) +
+    geom_line() +
+    geom_ribbon(aes(ymin = ci_low, ymax = ci_high), alpha = 0.2, fill = "dodgerblue") +
+    theme_minimal() +
+    geom_point(data = dd, aes(x = rugged_std, y = log_gdp_std, color = cont), size = 2) +
+    labs(
+        title = "Regression of Ruggedness and GDP",
+        x = "Ruggedness",
+        y = "Log GDP (as proportion of mean)", 
+        color = "Continent"
+    ) +
+    theme(legend.position = "bottom")
